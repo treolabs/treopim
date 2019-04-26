9549d0b17f16876faf85ad0c3531023c71d3b137
r.ratsun <r.ratsun@zinitsolutions.com>
2019-04-26 16:04:30 +0300

    75273

--- application/Espo/Modules/Pim/Migration/V4Dot0Dot0.php
+++ application/Espo/Modules/Pim/Migration/V4Dot0Dot0.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace Espo\Modules\Pim\Migration;
 
 use Treo\Core\Migration\AbstractMigration;
+use Espo\Core\Utils\Util;
 
 /**
  * Migration class for version 4.0.0
@@ -38,6 +39,9 @@ class V4Dot0Dot0 extends AbstractMigration
     {
         // drop triggers
         $this->dropTriggers();
+
+        // migrate channel attribute values
+        $this->migrateChannelAttributeValues();
     }
 
     /**
@@ -55,5 +59,53 @@ class V4Dot0Dot0 extends AbstractMigration
             ->getPDO()
             ->prepare($sql);
         $sth->execute();
+    }
+
+    /**
+     * Migrate channel attribute values
+     */
+    protected function migrateChannelAttributeValues()
+    {
+        $sql
+            = "SELECT cpav.*, p.id as product_id, a.id as attribute_id   
+                FROM channel_product_attribute_value AS cpav 
+                JOIN channel AS c ON c.id=cpav.channel_id 
+                JOIN product_attribute_value AS pav ON pav.id=cpav.product_attribute_id
+                JOIN product AS p ON p.id=pav.product_id
+                JOIN attribute AS a ON a.id=pav.attribute_id  
+                WHERE cpav.deleted=0 AND c.deleted=0 AND pav.deleted=0 AND p.deleted=0 AND a.deleted=0";
+
+        $sth = $this
+            ->getEntityManager()
+            ->getPDO()
+            ->prepare($sql);
+        $sth->execute();
+
+        $data = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (!empty($data)) {
+            foreach ($data as $row) {
+                $entity = $this->getEntityManager()->getEntity('ProductAttributeValue');
+                $entity->set('productId', $row['product_id']);
+                $entity->set('attributeId', $row['attribute_id']);
+                $entity->set('scope', 'Channel');
+                $entity->set('createdById', 'system');
+                $entity->set('createdAt', date("Y-m-d H:i:s"));
+                $entity->set('value', $row['value']);
+                foreach ($row as $key => $value) {
+                    if (strpos($key, 'value_') !== false) {
+                        $entity->set(Util::toCamelCase($key), $value);
+                    }
+                }
+
+                $this->getEntityManager()->saveEntity($entity, ['skipAll' => true]);
+
+                // relate
+                $this
+                    ->getEntityManager()
+                    ->getRepository('ProductAttributeValue')
+                    ->relate($entity, 'channels', $this->getEntityManager()->getEntity('Channel', $row['channel_id']));
+            }
+        }
     }
 }
