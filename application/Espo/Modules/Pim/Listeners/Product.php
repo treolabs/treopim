a7e11e91c302b35cc6356bd4328627a20c7fa127
r.zablodskyi <r.zablodskiy@zinitsolutions.com>
2019-05-13 10:28:55 +0300

    modification response data for attributes

---
+++ application/Espo/Modules/Pim/Listeners/Product.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Pim
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Espo\Modules\Pim\Listeners;
+
+use Treo\Listeners\AbstractListener;
+
+/**
+ * Class Product
+ *
+ * @author r.zablodskiy@treolabs.com
+ */
+class Product extends AbstractListener
+{
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    public function afterActionListLinked(array $data): array
+    {
+        if ($data['params']['link'] == 'productAttributeValues' && !empty($data['result']['list'])) {
+            $attributes = $this
+                ->getEntityManager()
+                ->getRepository('Attribute')
+                ->select(['id', 'typeValue'])
+                ->where([
+                    'id' => array_column($data['result']['list'], 'attributeId')
+                ])
+                ->find();
+
+            if (count($attributes) > 0) {
+                foreach ($attributes as $attribute) {
+                    foreach ($data['result']['list'] as $key => $item) {
+                        if ($item->attributeId == $attribute->get('id')) {
+                            $data['result']['list'][$key]->typeValue = $attribute->get('typeValue');
+                        }
+                    }
+                }
+            }
+        }
+
+        return $data;
+    }
+}
---
+++ application/Espo/Modules/Pim/Listeners/ProductAttributeValue.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Pim
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Espo\Modules\Pim\Listeners;
+
+use Espo\Core\Exceptions\Error;
+use Treo\Listeners\AbstractListener;
+
+/**
+ * Class ProductAttributeValue
+ *
+ * @author r.zablodskiy@treolabs.com
+ */
+class ProductAttributeValue extends AbstractListener
+{
+    /**
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws Error
+     */
+    public function afterActionRead(array $data): array
+    {
+        if (isset($data['result']->attributeId)) {
+            $attribute = $this->getEntityManager()->getEntity('Attribute', $data['result']->attributeId);
+
+            if (!empty($attribute)) {
+                $data['result']->typeValue = $attribute->get('typeValue');
+            }
+        }
+
+        return $data;
+    }
+}
