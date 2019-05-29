e9f39917bb249f573eb99c2b3526a4075c5ced26
Valeriy Shamota <v.shamota@zinitsolutions.com>
2019-05-29 16:20:56 +0300

    76028
    fix condition for overview filters

--- client/modules/pim/src/views/header.js
+++ client/modules/pim/src/views/header.js
@@ -21,7 +21,8 @@ Espo.define('pim:views/header', 'class-replace!pim:views/header',
     Dep => Dep.extend({
 
         setup() {
-            if (!this.baseOverviewFilters.find(filter => filter.name === 'channelsFilter') && this.model.name === 'Product' &&
+            if (this.model && !this.model.isNew() && this.getMetadata().get(['scopes', this.model.name, 'advancedFilters']) &&
+                !this.baseOverviewFilters.find(filter => filter.name === 'channelsFilter') && this.model.name === 'Product' &&
                 this.getAcl().check('ProductAttributeValue', 'read') && this.getAcl().check('Channel', 'read')) {
                 this.baseOverviewFilters.push({
                     name: 'channelsFilter',
