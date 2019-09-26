02e96881dd59380d1752845af16eee5c388c0614
r.ratsun <r.ratsun@gmail.com>
2019-09-26 11:25:04 +0300

    77481

---
+++ app/Listeners/PimImageEntity.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Pim
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Listeners;
+
+use Treo\Core\EventManager\Event;
+use Treo\Listeners\AbstractListener;
+
+/**
+ * Class PimImageEntity
+ *
+ * @author r.ratsun <r.ratsun@treolabs.com>
+ */
+class PimImageEntity extends AbstractListener
+{
+    /**
+     * @param Event $event
+     */
+    public function beforeSave(Event $event)
+    {
+        // get entity
+        $entity = $event->getArgument('entity');
+
+        // set entity name
+        $entity->set('name', $entity->get('image')->get('name'));
+    }
+}
