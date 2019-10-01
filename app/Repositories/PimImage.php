e90ff3d8390ed53cd0f4352a10c846c5446ff853
r.ratsun <r.ratsun@gmail.com>
2019-10-01 11:45:22 +0300

    77481

--- app/Repositories/PimImage.php
+++ app/Repositories/PimImage.php
@@ -20,7 +20,11 @@
 
 namespace Pim\Repositories;
 
+use Espo\Core\Exceptions\BadRequest;
+use Espo\Core\Exceptions\Error;
 use Espo\Core\Templates\Repositories\Base;
+use Espo\ORM\Entity;
+use Treo\Entities\Attachment;
 
 /**
  * Class PimImage
@@ -29,4 +33,102 @@ use Espo\Core\Templates\Repositories\Base;
  */
 class PimImage extends Base
 {
+    /**
+     * @inheritDoc
+     */
+    public function beforeSave(Entity $entity, array $options = [])
+    {
+        // call parent action
+        parent::beforeSave($entity, $options);
+
+        if ($entity->get('type') == 'Link' && !empty($entity->get('link'))) {
+            $attachment = $this->createAttachmentByLink($entity->get('link'));
+            $entity->set('imageId', $attachment->get('id'));
+            $entity->set('imageName', $attachment->get('name'));
+        }
+
+        // set entity name
+        $entity->set('name', $entity->get('imageName'));
+
+        // set sort order
+        $entity->set('sortOrder', time());
+
+        if (!$this->isUnique($entity)) {
+            throw new BadRequest('Such record already exists');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function init()
+    {
+        parent::init();
+
+        $this->addDependency('fileStorageManager');
+    }
+
+    /**
+     * @param string $link
+     *
+     * @return Attachment
+     * @throws Error
+     */
+    protected function createAttachmentByLink(string $link): Attachment
+    {
+        // get contents
+        if (empty($contents = @file_get_contents($link))) {
+            throw new Error('Wrong image link. Link: ' . $link);
+        }
+
+        // create attachment
+        $attachment = $this->getEntityManager()->getEntity('Attachment');
+        $attachment->set('name', array_pop(explode('/', $link)));
+        $attachment->set('field', 'image');
+        $attachment->set('role', 'Attachment');
+
+        // get file storage manager
+        $sm = $this->getInjection('fileStorageManager');
+
+        // store file
+        $sm->putContents($attachment, $contents);
+
+        // get mime type
+        $type = mime_content_type($sm->getLocalFilePath($attachment));
+
+        if (!in_array($type, ['image/jpeg', 'image/png', 'image/gif'])) {
+            $sm->unlink($attachment);
+            throw new Error('Wrong file mime type. Only image allowed. Link:' . $link);
+        }
+
+        // set mime type
+        $attachment->set('type', $type);
+
+        // save attachment
+        $this->getEntityManager()->saveEntity($attachment);
+
+        return $attachment;
+    }
+
+    /**
+     * @param Entity $entity
+     *
+     * @return bool
+     */
+    protected function isUnique(Entity $entity): bool
+    {
+        $count = $this
+            ->getEntityManager()
+            ->getRepository('PimImage')
+            ->where(
+                [
+                    'categoryId' => $entity->get('categoryId'),
+                    'productId'  => $entity->get('productId'),
+                    'imageId'    => $entity->get('imageId')
+                ]
+            )
+            ->count();
+
+        return empty($count);
+    }
 }
