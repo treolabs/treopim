245fa25830f84d43b54e35315844149c80658871
m.kokhanskyi <m.kokhanskyi@treolabs.com>
2019-11-09 11:49:23 +0200

    79365

---
+++ app/Listeners/AssetRelationEntity.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Pim
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Listeners;
+
+use Espo\Core\Exceptions\Error;
+use Treo\Core\EventManager\Event;
+use Treo\Listeners\AbstractListener;
+
+/**
+ * Class AssetRelationEntity
+ * @package Pim\Listeners
+ *
+ * @author m.kokhanskyi <m.kokhanskyi@treolabs.com>
+ */
+class AssetRelationEntity extends AbstractListener
+{
+    protected $hasMainImage = ['Product', 'Category'];
+
+    /**
+     * @param Event $event
+     */
+    public function afterSave(Event $event)
+    {
+        $assetRelation = $event->getArgument('entity');
+        if (!empty($assetRelation)) {
+            $this->getService('AssetRelation')
+                ->updateMainImage($assetRelation->get('entityName'), $assetRelation->get('entityId'));
+        }
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function afterRemove(Event $event)
+    {
+        $assetRelation = $event->getArgument('entity');
+        if (!empty($assetRelation)) {
+            $this->getService('AssetRelation')
+                ->updateMainImage($assetRelation->get('entityName'), $assetRelation->get('entityId'));
+        }
+    }
+}
---
+++ app/Services/AssetRelation.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Pim
+ * Free Extension
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Services;
+
+use Dam\Services\AssetRelation as AssetRelationDam;
+use Espo\Core\Exceptions\Error;
+use PDO;
+
+/**
+ * Class AssetRelation
+ * @package Pim\Services
+ *
+ * @author m.kokhanskyi <m.kokhanskyi@treolabs.com>
+ */
+class AssetRelation extends AssetRelationDam
+{
+    protected $hasMainImage = ['Product', 'Category'];
+
+    /**
+     * @param string $entityName
+     * @param string $entityId
+     */
+    public function updateMainImage(string $entityName, string $entityId): void
+    {
+        if (in_array($entityName, $this->hasMainImage)) {
+            $foreign = $this->getEntityManager()->getEntity($entityName, $entityId);
+            // find first image
+            $imageId = $this->getEntityManager()
+                ->nativeQuery(
+                    "SELECT a.file_id
+                            FROM asset a
+                                RIGHT JOIN asset_relation ar ON ar.asset_id = a.id
+                            WHERE ar.entity_name = '{$entityName}' AND ar.entity_id = '{$entityId}'
+                            ORDER BY ar.sort_order, ar.created_at")
+                ->fetch(PDO::FETCH_COLUMN);
+
+            // update main image if it needs
+            if (!empty($foreign) && $imageId != $foreign->get('imageId')) {
+                $foreign->set('imageId', $imageId);
+                // set keep attachment param
+                $foreign->keepAttachment = true;
+                $this->getEntityManager()->saveEntity($foreign);
+            }
+        }
+    }
+
+    /**
+     * @param $assetId
+     * @return string|null
+     * @throws Error
+     */
+    protected function getAttachmentId($assetId): ?string
+    {
+        $asset = $this->getEntityManager()->getEntity('Asset', $assetId);
+        return !empty($asset) ? $asset->get('fileId') : null;
+    }
+}
