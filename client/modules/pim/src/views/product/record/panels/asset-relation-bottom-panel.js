9026224d5ec391b7fc168404fe5b0e919f66a856
m.kokhanskyi <Dell6789345>
2019-12-02 16:22:54 +0200

    79743

--- client/modules/pim/src/views/product/record/panels/asset-relation-bottom-panel.js
+++ client/modules/pim/src/views/product/record/panels/asset-relation-bottom-panel.js
@@ -1,11 +1,15 @@
 Espo.define('pim:views/product/record/panels/asset-relation-bottom-panel', 'dam:views/asset_relation/record/panels/bottom-panel',
     Dep => Dep.extend({
+        additionalData: {},
+
         _createTypeBlock(model, show, callback) {
-            model.set({
+            let data = {
                 entityName: this.defs.entityName,
                 entityId  : this.model.id,
                 entityModel  : this.model
-            });
+            };
+            model.set({...data ,...this.additionalData});
+
             this.createView(model.get('name'), "pim:views/product/record/panels/asset-type-block", {
                 model: model,
                 el   : this.options.el + ' .group[data-name="' + model.get("name") + '"]',
