2810bccaf000af1aeced00dfe0df0264c2261ba2
r.ratsun <r.ratsun@gmail.com>
2019-12-19 16:20:41 +0200

    80060

--- client/modules/pim/src/views/attribute/record/row-actions/default.js
+++ client/modules/pim/src/views/attribute/record/row-actions/default.js
@@ -22,33 +22,15 @@ Espo.define('pim:views/attribute/record/row-actions/default', 'views/record/row-
     return Dep.extend({
 
         getActionList: function () {
-            var list = [{
-                action: 'quickView',
-                label: 'View',
-                data: {
-                    id: this.model.id
-                },
-                link: '#' + this.model.name + '/view/' + this.model.id
-            }];
-            if (this.options.acl.edit) {
-                list.push({
-                    action: 'quickEdit',
-                    label: 'Edit',
-                    data: {
-                        id: this.model.id
-                    },
-                    link: '#' + this.model.name + '/edit/' + this.model.id
-                });
-            }
-            if (this.options.acl.delete) {
-                list.push({
-                    action: 'quickRemove',
-                    label: 'Remove',
-                    data: {
-                        id: this.model.id
-                    }
-                });
-            }
+            // get locale
+            let locale = this.model.get('locale');
+
+            let list = [];
+            $.each(Dep.prototype.getActionList.call(this), function () {
+                if (locale === null || (locale !== null && this.action !== 'quickRemove')) {
+                    list.push(this);
+                }
+            });
 
             return list;
         }
