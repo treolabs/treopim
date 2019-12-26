472b4bd890c6b8428ab62321307c818a33e7cea6
r.ratsun <r.ratsun@gmail.com>
2019-12-26 15:28:26 +0200

    80060

--- app/Migrations/V3Dot13Dot0.php
+++ app/Migrations/V3Dot13Dot0.php
@@ -36,11 +36,18 @@ class V3Dot13Dot0 extends Base
      */
     public function up(): void
     {
-        $this->exec("ALTER TABLE attribute ADD locale VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
-        $this->exec("ALTER TABLE attribute ADD parent_id VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+        /**
+         * Migrate Attribute DB schema
+         */
         $this->exec("ALTER TABLE attribute DROP is_system");
+        $this->exec("ALTER TABLE attribute ADD locale VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+        $this->exec("CREATE INDEX IDX_LOCALE ON `attribute` (locale, deleted)");
+        $this->exec("ALTER TABLE attribute ADD parent_id VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
         $this->exec("CREATE INDEX IDX_PARENT_ID ON attribute (parent_id)");
 
+        /**
+         * Migrate ProductFamilyAttribute DB schema
+         */
         $this->exec("DROP INDEX IDX_NAME ON `product_family_attribute`");
         $this->exec("ALTER TABLE `product_family_attribute` DROP name");
         $this->exec("ALTER TABLE `product_family_attribute` ADD attribute_type VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
@@ -52,7 +59,11 @@ class V3Dot13Dot0 extends Base
         $this->exec("ALTER TABLE `product_family_attribute` ADD parent_id VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
         $this->exec("CREATE INDEX IDX_PARENT_ID ON `product_family_attribute` (parent_id)");
         $this->exec("ALTER TABLE `product_family_attribute` ADD locale VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+        $this->exec("CREATE INDEX IDX_LOCALE ON `product_family_attribute` (locale, deleted)");
 
+        /**
+         * Migrate ProductAttributeValue DB schema
+         */
         $this->exec("DROP INDEX IDX_NAME ON `product_attribute_value`");
         $this->exec("ALTER TABLE `product_attribute_value` DROP name");
         $this->exec("ALTER TABLE `product_attribute_value` ADD attribute_type VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
@@ -61,7 +72,11 @@ class V3Dot13Dot0 extends Base
         $this->exec("CREATE INDEX IDX_ATTRIBUTE ON `product_attribute_value` (attribute_id, deleted)");
         $this->exec("CREATE INDEX IDX_SCOPE ON `product_attribute_value` (scope, deleted)");
         $this->exec("ALTER TABLE `product_attribute_value` ADD locale VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+        $this->exec("CREATE INDEX IDX_LOCALE ON `product_attribute_value` (locale, deleted)");
 
+        /**
+         * Migrate DATA
+         */
         $attributes = $this->fetchAll("SELECT id, type, locale FROM attribute WHERE deleted=0");
         foreach ($attributes as $attribute) {
             /** @var string $id */
