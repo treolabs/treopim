065448fba50430c28cbb4f8bc5ef1449ac73a818
r.ratsun <r.ratsun@gmail.com>
2019-12-27 15:29:13 +0200

    80060

--- app/Migrations/V3Dot13Dot0.php
+++ app/Migrations/V3Dot13Dot0.php
@@ -207,7 +207,7 @@ class V3Dot13Dot0 extends Base
         try {
             $this->getPDO()->exec($sql);
         } catch (\PDOException $e) {
-            $GLOBALS['log']->error('Migration of PIM (3.13.0): ' . $sql . ' ' . $e->getMessage());
+            $GLOBALS['log']->error('Migration of PIM (3.13.0): ' . $sql . ' | ' . $e->getMessage());
             $this->errors++;
         }
     }
