27b0da8637ec2ed7b329bf51d6ca04e766e15c61
r.ratsun <r.ratsun@gmail.com>
2020-01-30 14:24:49 +0200

    80703

--- app/Migrations/V3Dot13Dot1.php
+++ app/Migrations/V3Dot13Dot1.php
@@ -72,6 +72,32 @@ class V3Dot13Dot1 extends Base
      */
     public function down(): void
     {
+        echo ' Create DB table `product_category`... ';
+        $this->exec(
+            "CREATE TABLE `product_category` (`id` VARCHAR(24) NOT NULL COLLATE utf8mb4_unicode_ci, `name` VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `deleted` TINYINT(1) DEFAULT '0' COLLATE utf8mb4_unicode_ci, `sorting` INT DEFAULT '0' COLLATE utf8mb4_unicode_ci, `scope` VARCHAR(255) DEFAULT 'Global' COLLATE utf8mb4_unicode_ci, `created_at` DATETIME DEFAULT NULL COLLATE utf8mb4_unicode_ci, `modified_at` DATETIME DEFAULT NULL COLLATE utf8mb4_unicode_ci, `product_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `category_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `created_by_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `modified_by_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `owner_user_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `assigned_user_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, INDEX `IDX_PRODUCT_ID` (product_id), INDEX `IDX_CATEGORY_ID` (category_id), INDEX `IDX_CREATED_BY_ID` (created_by_id), INDEX `IDX_MODIFIED_BY_ID` (modified_by_id), INDEX `IDX_OWNER_USER_ID` (owner_user_id), INDEX `IDX_ASSIGNED_USER_ID` (assigned_user_id), INDEX `IDX_OWNER_USER` (owner_user_id, deleted), INDEX `IDX_ASSIGNED_USER` (assigned_user_id, deleted), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB"
+        );
+        echo ' Done!' . PHP_EOL;
+
+        echo ' Create DB table `product_category_channel`... ';
+        $this->exec(
+            "CREATE TABLE `product_category_channel` (`id` INT AUTO_INCREMENT NOT NULL UNIQUE COLLATE utf8mb4_unicode_ci, `channel_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `product_category_id` VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci, `deleted` TINYINT(1) DEFAULT '0' COLLATE utf8mb4_unicode_ci, INDEX `IDX_EBADAAC572F5A1AA` (channel_id), INDEX `IDX_EBADAAC5BE6903FD` (product_category_id), UNIQUE INDEX `UNIQ_EBADAAC572F5A1AABE6903FD` (channel_id, product_category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB"
+        );
+        echo ' Done!' . PHP_EOL;
+
+        echo ' Migrate DATA: ' . PHP_EOL;
+        $pcs = $this->fetchAll("SELECT id,product_id,category_id,sorting FROM product_category_linker WHERE deleted=0");
+        foreach ($pcs as $pc) {
+            $this->exec("INSERT INTO product_category (id,product_id,category_id,sorting) VALUES (:id,:product_id,:category_id,:sorting)", $pc);
+        }
+        echo ' Done!' . PHP_EOL;
+
+        echo ' DROP DB table `product_category_linker`... ';
+        $this->exec("DROP TABLE product_category_linker");
+        echo ' Done!' . PHP_EOL;
+
+        if (!empty($this->errors)) {
+            echo ' ' . $this->errors . ' requests failed. Please, refer to the log file for details.' . PHP_EOL;
+        }
     }
 
     /**
